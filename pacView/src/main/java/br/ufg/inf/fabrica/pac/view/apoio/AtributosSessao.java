d8b4bb506cc1e1f5d897607adf69bdde246d182b
danilloguimaraes <danillo@danilloguimaraes.com.br>
2016-03-31 13:52:03 -0300

    Classe com constantes que definem nome de atributos na sessão

---
+++ pacView/src/main/java/br/ufg/inf/fabrica/pac/view/apoio/AtributosSessao.java
@@ -0,0 +1,25 @@
+package br.ufg.inf.fabrica.pac.view.apoio;
+
+import java.util.List;
+
+/**
+ *
+ * @author Danillo
+ */
+public class AtributosSessao {
+
+    /**
+     * Atributo salvo na sessão que representa o usuário autenticado
+     */
+    public final static String USUARIO_LOGADO = "usuarioLogado";
+    
+    /**
+     * Atributo salvo na sessão que representa o projeto selecionado no sistema
+     */
+    public final static String PROJETO_SELECIONADO = "projetoSelecionado";
+    
+    /**
+     * Atributo salvo na sessão que representa as mensagens de erro
+     */
+    public final static String MENSAGENS_DE_ERRO = "errorMessages";
+}
