e3cfcd62a18cbfaeae0b9db9a9aed07e067fd662
danilloguimaraes <danillo@danilloguimaraes.com.br>
2016-04-01 10:59:28 -0300

    Adição de método para configurar uma mensagem de erro e direcionar para respectiva página de erro

--- pacView/src/main/java/br/ufg/inf/fabrica/pac/view/apoio/util/UtilVisao.java
+++ pacView/src/main/java/br/ufg/inf/fabrica/pac/view/apoio/util/UtilVisao.java
@@ -1,6 +1,9 @@
 package br.ufg.inf.fabrica.pac.view.apoio.util;
 
+import br.ufg.inf.fabrica.pac.view.apoio.AtributosSessao;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,5 +19,16 @@ public class UtilVisao {
             HttpServletResponse response, String destino) throws ServletException, IOException{
         RequestDispatcher dispatcher = request.getRequestDispatcher(destino);
         dispatcher.forward(request, response);
+    }
+    
+    public static void direcionarPaginaErro(HttpServletRequest request, 
+            HttpServletResponse response, String mensagemErro){
+        RequestDispatcher dispatcher = request.getRequestDispatcher("erro.jsp");
+        request.getSession().setAttribute(AtributosSessao.MENSAGENS_DE_ERRO, mensagemErro);
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(UtilVisao.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
