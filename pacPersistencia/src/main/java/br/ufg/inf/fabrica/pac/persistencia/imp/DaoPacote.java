11342741aed39bf88f5f8e3e4605d7f633684785
danilloguimaraes <danillo@danilloguimaraes.com.br>
2016-04-07 10:13:44 -0300

    Em DaoAndamento e DaoPacote foram removidos os tratamento de exceções e os métodos passaram a lançar a exceção por meio da declaração throws

--- pacPersistencia/src/main/java/br/ufg/inf/fabrica/pac/persistencia/imp/DaoPacote.java
+++ pacPersistencia/src/main/java/br/ufg/inf/fabrica/pac/persistencia/imp/DaoPacote.java
@@ -15,94 +15,80 @@ import java.util.logging.Logger;
  *
  * @author Danillo
  */
-public class DaoPacote implements IDaoPacote{
+public class DaoPacote implements IDaoPacote {
 
     @Override
-    public Pacote salvar(Pacote entity, Transacao transacao) {
+    public Pacote salvar(Pacote entity, Transacao transacao) throws SQLException {
 
-        
-        String sqlUpdate = "update PACOTE set Abandonado=?, DataCriacao=?, DataPrevistaRealizacao=?" +
-            ", Descricao=?, Documento=?, nomeEstado=?, IdProjeto=?, IdUsuario=?" +
-            ", Nome=? where id = ?";
+        String sqlUpdate = "update PACOTE set Abandonado=?, DataCriacao=?, DataPrevistaRealizacao=?"
+                + ", Descricao=?, Documento=?, nomeEstado=?, IdProjeto=?, IdUsuario=?"
+                + ", Nome=? where id = ?";
         String sqlInsert = "insert into PACOTE (Abandonado, DataCriacao, "
                 + "DataPrevistaRealizacao, Descricao, Documento, nomeEstado, "
                 + "IdProjeto, IdUsuario, Nome) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        try {
-            PreparedStatement pst;
-            if(entity.getId()==0){
-                pst = Conexao.getConnection().prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
-            } else {
-                pst = Conexao.getConnection().prepareStatement(sqlUpdate);
-                pst.setLong(10, entity.getId());
-            }
-            
-            pst.setBoolean(1, entity.isAbandonado());
-            pst.setDate(2, Utils.convertUtilDateToSqlDate(entity.getDataCriacao()));
-            pst.setDate(3, Utils.convertUtilDateToSqlDate(entity.getDataPrevistaRealizacao()));
-            pst.setString(4, entity.getDescricao());
-            pst.setString(5, entity.getDocumento());
-            pst.setString(6, entity.getNomeEstado());
-            pst.setLong(7, entity.getIdProjeto());
-            pst.setLong(8, entity.getIdUsuario());
-            pst.setString(9, entity.getNome());
-            
-            pst.execute();
-            if(entity.getId()==0){
-                ResultSet keys = pst.getGeneratedKeys();
-                if(keys.next()){
-                    entity.setId(keys.getInt(1));
-                }
-            }
-            return entity;
-        } catch (SQLException ex) {
-            Logger.getLogger(DaoPacote.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+        PreparedStatement pst;
+        if (entity.getId() == 0) {
+            pst = Conexao.getConnection().prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+        } else {
+            pst = Conexao.getConnection().prepareStatement(sqlUpdate);
+            pst.setLong(10, entity.getId());
         }
+
+        pst.setBoolean(1, entity.isAbandonado());
+        pst.setDate(2, Utils.convertUtilDateToSqlDate(entity.getDataCriacao()));
+        pst.setDate(3, Utils.convertUtilDateToSqlDate(entity.getDataPrevistaRealizacao()));
+        pst.setString(4, entity.getDescricao());
+        pst.setString(5, entity.getDocumento());
+        pst.setString(6, entity.getNomeEstado());
+        pst.setLong(7, entity.getIdProjeto());
+        pst.setLong(8, entity.getIdUsuario());
+        pst.setString(9, entity.getNome());
+
+        pst.execute();
+        if (entity.getId() == 0) {
+            ResultSet keys = pst.getGeneratedKeys();
+            if (keys.next()) {
+                entity.setId(keys.getInt(1));
+            }
+        }
+        return entity;
+
     }
 
     @Override
-    public Pacote excluir(Pacote entity, Transacao transacao) {
+    public Pacote excluir(Pacote entity, Transacao transacao) throws SQLException {
         String sql = "delete from PACOTE where id=?";
-        try {
-            PreparedStatement pst;
-            pst = Conexao.getConnection().prepareStatement(sql);
-            pst.setLong(1, entity.getId());
-            pst.execute();
-            return entity;
-        } catch (SQLException ex) {
-            Logger.getLogger(DaoPacote.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
+        PreparedStatement pst;
+        pst = Conexao.getConnection().prepareStatement(sql);
+        pst.setLong(1, entity.getId());
+        pst.execute();
+        return entity;
     }
 
     @Override
-    public Pacote buscar(long id) {
+    public Pacote buscar(long id) throws SQLException {
         String sql = "select P.* from PACOTE as P where id=?";
-        try {
-            PreparedStatement pst;
-            pst = Conexao.getConnection().prepareStatement(sql);
-            pst.setLong(1, id);
-            ResultSet rs = pst.executeQuery();
-            Pacote pacote = null;
-            if (rs.next()){
-                pacote = new Pacote();
-                pacote.setId(rs.getLong("id"));
-                pacote.setAbandonado(rs.getBoolean("abandonado"));
-                pacote.setDataCriacao(rs.getDate("dataCriacao"));
-                pacote.setDataPrevistaRealizacao(rs.getDate("dataPrevistaRealizacao"));
-                pacote.setDescricao(rs.getString("descricao"));
-                pacote.setDocumento(rs.getString("documento"));
-                pacote.setId(rs.getLong("id"));
-                pacote.setNomeEstado(rs.getString("nomeEstado"));
-                pacote.setIdProjeto(rs.getLong("idProjeto"));
-                pacote.setIdUsuario(rs.getLong("idUsuario"));
-                pacote.setNome(rs.getString("nome"));
-            }
-            return pacote;
-        } catch (SQLException ex) {
-            Logger.getLogger(DaoPacote.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+
+        PreparedStatement pst;
+        pst = Conexao.getConnection().prepareStatement(sql);
+        pst.setLong(1, id);
+        ResultSet rs = pst.executeQuery();
+        Pacote pacote = null;
+        if (rs.next()) {
+            pacote = new Pacote();
+            pacote.setId(rs.getLong("id"));
+            pacote.setAbandonado(rs.getBoolean("abandonado"));
+            pacote.setDataCriacao(rs.getDate("dataCriacao"));
+            pacote.setDataPrevistaRealizacao(rs.getDate("dataPrevistaRealizacao"));
+            pacote.setDescricao(rs.getString("descricao"));
+            pacote.setDocumento(rs.getString("documento"));
+            pacote.setId(rs.getLong("id"));
+            pacote.setNomeEstado(rs.getString("nomeEstado"));
+            pacote.setIdProjeto(rs.getLong("idProjeto"));
+            pacote.setIdUsuario(rs.getLong("idUsuario"));
+            pacote.setNome(rs.getString("nome"));
         }
+        return pacote;
     }
-    
+
 }
